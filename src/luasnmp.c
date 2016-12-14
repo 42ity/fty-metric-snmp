26958aad09ca5cf7d0809ff6b54e58ef4ca82cc2
Tomas Halman <tomas@halman.net>
2016-12-14 20:45:03 +0100

    Problem: need getnext c function

--- src/luasnmp.c
+++ src/luasnmp.c
@@ -26,18 +26,19 @@ char *oid_to_sring (myoid anOID[], int len)
     return strdup (buffer);
 }
 
-char *var_to_sring (const oid *objid, size_t objidlen, const netsnmp_variable_list *variable)
+char *var_to_sring (const netsnmp_variable_list *variable)
 {
     char buffer[1024];
     netsnmp_ds_set_boolean(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_QUICK_PRINT, true);
     memset (buffer, 0, sizeof (buffer));
-    if(snprint_value(buffer, sizeof(buffer)-1, objid, objidlen, variable) == -1) return NULL;
+    if(snprint_value(buffer, sizeof(buffer)-1, variable->name, variable->name_length, variable) == -1) return NULL;
     return strdup (buffer);
 }
 
 
 
-char* snmp_get_v12 (const char* host, const char *oid, const char* community, int version) {
+char* snmp_get_v12 (const char* host, const char *oid, const char* community, int version)
+{
     struct snmp_session session, *ss;
     struct snmp_pdu *pdu;
     struct snmp_pdu *response;
@@ -65,24 +66,27 @@ char* snmp_get_v12 (const char* host, const char *oid, const char* community, in
    
     if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
         vars = response->variables;
-        result = var_to_sring (vars->name, vars->name_length, vars);
+        result = var_to_sring (vars);
     }
     if (response) snmp_free_pdu (response);
     snmp_close(ss);
     return result;
 }
 
-bool snmp_getnext_v12 (const char* host, const char *oid, const char* community, int version, char **resultoid, char **resultvalue) {
+void snmp_getnext_v12 (const char* host, const char *oid, const char* community, int version, char **resultoid, char **resultvalue)
+{
     struct snmp_session session, *ss;
     struct snmp_pdu *pdu;
     struct snmp_pdu *response;
-    char *result = NULL;
     unsigned long int anOID[MAX_OID_LEN];
     size_t anOID_len = MAX_OID_LEN;
-    char buffer[1024];
-    
+    char *nextoid = NULL;
+    char *nextvalue = NULL; 
     struct variable_list *vars;
     int status;
+
+    *resultoid = NULL;
+    *resultvalue = NULL;        
 
     snmp_sess_init (&session);
     session.peername = (char *)host;
@@ -91,7 +95,7 @@ bool snmp_getnext_v12 (const char* host, const char *oid, const char* community,
     session.community_len = strlen (community);
    
     ss = snmp_open (&session);
-    if (!ss) return false;
+    if (!ss) return;
     
     pdu = snmp_pdu_create (SNMP_MSG_GETNEXT);
     read_objid (oid, anOID, &anOID_len);
@@ -102,37 +106,30 @@ bool snmp_getnext_v12 (const char* host, const char *oid, const char* community,
     if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
         
         vars = response->variables; // we should have just one variable
-        snprint_objid(buffer, 1024, vars->name, vars->name_length);
-        
-        switch (vars->type) {
-        case ASN_OCTET_STR:
-            result = malloc(1 + vars->val_len);
-            memcpy(result, vars->val.string, vars->val_len);
-            result[vars->val_len] = '\0';
-            break;
-        case ASN_INTEGER:
-        case ASN_GAUGE:
-        case ASN_COUNTER:
-        case ASN_TIMETICKS:
-            if (asprintf (&result, "%ld", *vars->val.integer) == -1) result = false;
-            break;
-        default:
-            result = false;
-            break;
-        }
+        nextoid = oid_to_sring (vars->name, vars->name_length);
+        nextvalue = var_to_sring (vars);
     }
-       
+    if (nextoid && nextvalue) {
+        *resultoid = nextoid;
+        *resultvalue = nextvalue;
+    } else {
+        if (resultoid) free (resultoid);
+        if (resultvalue) free (resultvalue);
+        *resultoid = NULL;
+        *resultvalue = NULL;        
+    }
     if (response) snmp_free_pdu (response);
     snmp_close(ss);
-    return result;
 }
 
 
-void luasnmp_init() {
+void luasnmp_init()
+{
     init_snmp("fty-snmp-client");
 }
 
-static int snmp_get(lua_State *L){
+static int snmp_get(lua_State *L)
+{
 	char* host = lua_tostring(L, 1);
 	char* oid = lua_tostring(L, 2);
     //printf ("lua is calling %s %s\n", host, oid);
@@ -147,19 +144,25 @@ static int snmp_get(lua_State *L){
     }
 }
 
-void extend_lua_of_snmp(lua_State *L) {
+void extend_lua_of_snmp(lua_State *L)
+{
     lua_register (L, "snmp_get", snmp_get);
     //TODO: lua_register (L, "snmp_getnext", snmp_getnext);
 }
 
-int main() {
+int main()
+{
     luasnmp_init ();
     char *value = snmp_get_v12 ("10.130.53.36", ".1.3.6.1.2.1.1.1.0", "public", SNMP_VERSION_1);
     printf("value: %s\n", value ? value : "null");
     free (value);
 
     // lua snmp test
+#if LUA_VERSION_NUM > 501
+    lua_State *l = luaL_newstate();   
+#else
     lua_State *l = lua_open();
+#endif
     luaL_openlibs(l); // get functions like print();
     extend_lua_of_snmp (l); //extend of snmp
     luaL_dostring (l, "function main(host) print (snmp_get (host,'.1.3.6.1.2.1.1.1.0')) end");
